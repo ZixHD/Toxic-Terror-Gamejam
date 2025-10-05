298bbe4e59405eddd702892152ed44af79a2429a
ZixHD <havos25@gmail.com>
2025-10-05 23:54:00 +0200

    added random animation in idle

--- toxic-terror-gamejam/Utils/StatePattern/IdleLeft.gd
+++ toxic-terror-gamejam/Utils/StatePattern/IdleLeft.gd
@@ -2,11 +2,15 @@ extends State
 class_name IdleLeft
 
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
-
+var rng = RandomNumberGenerator.new()
 
 func Enter():
 	#anim play idle
-	animation_player.play("idle1L")
+	var num = rng.randi_range(1, 2)
+	if num == 1:
+		animation_player.play("idle1L")
+	else:
+		animation_player.play("idle2L")
 	pass
 
 func Physics_Update(_delta: float):
--- toxic-terror-gamejam/Utils/StatePattern/IdleRight.gd
+++ toxic-terror-gamejam/Utils/StatePattern/IdleRight.gd
@@ -2,11 +2,15 @@ extends State
 class_name IdleRight
 
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
-
+var rng = RandomNumberGenerator.new()
 
 func Enter():
 	#anim play idle
-	animation_player.play("idle1R")
+	var num = rng.randi_range(1, 2)
+	if num == 1:
+		animation_player.play("idle1R")
+	else:
+		animation_player.play("idle2R")
 	pass
 
 func Physics_Update(_delta: float):
