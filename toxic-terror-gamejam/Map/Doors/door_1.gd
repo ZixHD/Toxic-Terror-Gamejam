9583582827f2c529e9b8e80cbe687057a4bd08f0
ZixHD <havos25@gmail.com>
2025-10-15 01:14:59 +0200

    added ascend and descend animations

--- toxic-terror-gamejam/Map/Doors/door_1.gd
+++ toxic-terror-gamejam/Map/Doors/door_1.gd
@@ -1,28 +1,27 @@
 extends Area2D
 
 @onready var main_layer: TileMapLayer = $"../../Layers/MainLayer"
-@onready var side_rooms: TileMapLayer = $"../../Layers/SideRooms"
-var side_rooms_flag: bool = false
+@onready var side_room_1: Area2D = $"../../Layers/SideRooms/SideRoom1"
+
 var room_entered: bool = false
 
 func _ready() -> void:
 	pass
 	
 func _physics_process(_delta: float) -> void:
-	print(room_entered)
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		var tween = create_tween()
-		
+		side_room_1.visible = true
 		tween.tween_property(main_layer, "modulate:a", 0.2, 0.1)
 
 
 		
 
 func _on_body_exited(body: Node2D) -> void:
-		
+		side_room_1.visible = false
 		if body.is_in_group("Player"):
 			var tween = create_tween()
 			if not room_entered:
@@ -38,7 +37,6 @@ func _on_body_exited(body: Node2D) -> void:
 
 func _on_side_room_1_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		print("uslo")
 		room_entered = true
 	pass # Replace with function body.
 
